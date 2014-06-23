9fafc9c08294763a6c52055984e88dc7eb72c384
Aaron L <aaron@bettercoder.net>
2014-06-23 15:37:17 -0700

    Initial commit.

---
+++ json.go
@@ -0,0 +1,230 @@
+package jsonware
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"reflect"
+	"strings"
+)
+
+type Thing struct {
+	Name string `json:"name"`
+}
+
+var count = 0
+
+/*
+JSONHandler handles json api endpoint restful requests. It can be constructed
+by passing a suitable function into the JSON function.
+
+JSONHandler
+*/
+type JSONHandler struct {
+	logger io.Writer
+	fn     reflect.Value
+	in     reflect.Type
+}
+
+// Log sets the JSONHandler's logging io.Writer for writing out cloaked errors.
+func (j *JSONHandler) Log(logger io.Writer) *JSONHandler {
+	j.logger = logger
+	return j
+}
+
+/*
+JSONErr can be used in a JSONHandler to override the error mechanism in
+JSONHandler's ServeHTTP method. If a status is set it will obey it,
+otherwise it will assume 200 OK. The error message will be relayed to the
+client. If you wish to use a general server error, simply return error from
+the handler.
+
+	func handler(w http.ResponseWriter, r *http.Request) {
+		return nil, nil // No response from ServeHTTP, use w to create one.
+	}
+
+	func handler(w http.ResponseWriter, r *http.Request) {
+		return nil, errors.New("hi") // 500 Response with cloaked+logged error.
+	}
+
+	func handler(w http.ResponseWriter, r *http.Request) {
+		return nil, JSONErr{error: errors.New("hi")} // 200 Response with error output to client
+	}
+
+	func handler(w http.ResponseWriter, r *http.Request) {
+		return nil, JSONErr{400, errors.New("hi")} // 400 Response with error output to client
+	}
+*/
+type JSONErr struct {
+	Status int
+	error
+}
+
+// ServeHTTP serves an http response, see JSONHandler documentation for details.
+func (j JSONHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// Ensure request accepts json
+	ah := r.Header.Get("Accept")
+	if !strings.Contains(ah, "*/*") && !strings.Contains(ah, "application/json") {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusBadRequest)
+		io.WriteString(w, "this endpoint only responds to json-accepting clients")
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	// Ensure request follows REST principles.
+	deserialize := j.fn.Type().NumIn() == 3
+	switch {
+	case deserialize && !isDataMethod(r.Method):
+		fallthrough
+	case !deserialize && isDataMethod(r.Method):
+		writeJSONError(w, j.logger, JSONErr{
+			http.StatusBadRequest,
+			fmt.Errorf("invalid http method to this endpoint: %s", r.Method),
+		})
+		return
+	}
+
+	// Set up arguments for handler call.
+	in := []reflect.Value{
+		reflect.ValueOf(w), reflect.ValueOf(r),
+	}
+	var deserializeTo reflect.Value
+	if deserialize {
+		switch j.in.Kind() {
+		case reflect.Slice, reflect.Map:
+			deserializeTo = reflect.New(j.in)
+			in = append(in, deserializeTo.Elem())
+		case reflect.Ptr:
+			deserializeTo = reflect.New(j.in.Elem())
+			in = append(in, deserializeTo)
+		}
+	}
+
+	// Do json deserialization of body.
+	if deserialize {
+		dec := json.NewDecoder(r.Body)
+		defer r.Body.Close()
+
+		if err := dec.Decode(deserializeTo.Interface()); err != nil {
+			writeJSONError(w, j.logger, JSONErr{
+				http.StatusBadRequest,
+				fmt.Errorf("could not deserialize json request body"),
+			})
+			return
+		}
+	}
+
+	out := j.fn.Call(in)
+
+	// Handle error return value
+	if !out[1].IsNil() {
+		writeJSONError(w, j.logger, out[1].Interface().(error))
+		return
+	}
+
+	// Serialize the interface{} return value
+	if !out[0].IsNil() {
+		enc := json.NewEncoder(w)
+		if err := enc.Encode(out[0].Interface()); err != nil {
+			writeJSONError(w, j.logger, JSONErr{
+				http.StatusInternalServerError,
+				fmt.Errorf("problem preparing response"),
+			})
+			return
+		}
+	}
+}
+
+// isDataMethod returns true if method is one of POST PUT or PATCH
+func isDataMethod(method string) bool {
+	return method == "POST" || method == "PATCH" || method == "PUT"
+}
+
+// jsonSanitizer does basic sanitization (except unicode) of json values.
+var jsonSanitizer = strings.NewReplacer(
+	`"`, `\"`, `\`, `\\`, `/`, `\/`, "\x08", "\\b",
+	"\x12", "\\f", "\n", "\\n", "\r", "\\r", "\t", "\\t",
+)
+
+// writeJSONError writes an error out to the response.
+func writeJSONError(w http.ResponseWriter, logger io.Writer, err error) {
+	switch e := err.(type) {
+	case JSONErr:
+		if e.Status != 0 {
+			w.WriteHeader(e.Status)
+		} else {
+			w.WriteHeader(http.StatusOK)
+		}
+		fmt.Fprintf(w, `{ "error": "%v" }`, jsonSanitizer.Replace(e.Error()))
+	default:
+		if logger != nil {
+			fmt.Fprintf(logger, "internal error: %v", err)
+		}
+		w.WriteHeader(http.StatusInternalServerError)
+		io.WriteString(w, `{ "error": "an internal server error occurred" }`)
+	}
+}
+
+/*
+JSON changes a function into a JSONHandler.
+Acceptable forms of the input function:
+
+	GET (Note: all variant return types also work with POST/PATCH/PUT
+	func Fn(w http.ResponseWriter, r *http.Request) (interface{}, error)
+	func Fn(w http.ResponseWriter, r *http.Request) (*MyStruct, error)
+	func Fn(w http.ResponseWriter, r *http.Request) ([]*MyStruct, error)
+	func Fn(w http.ResponseWriter, r *http.Request) (map[string]*MyStruct, error)
+
+	POST/PUT/PATCH
+	func Fn(w http.ResponseWriter, r *http.Request, m *MyStruct) (interface{}, error)
+	func Fn(w http.ResponseWriter, r *http.Request, m []*MyStruct) (interface{}, error)
+	func Fn(w http.ResponseWriter, r *http.Request, m map[string]*MyStruct) (interface{}, error)
+*/
+func JSON(fn interface{}) *JSONHandler {
+	typ := reflect.TypeOf(fn)
+	if typ.Kind() != reflect.Func {
+		panic("Can only register functions.")
+	}
+
+	var p1, p2, p3 reflect.Type
+
+	switch typ.NumIn() {
+	case 3:
+		p3 = typ.In(2)
+		if p3.Kind() != reflect.Ptr && p3.Kind() != reflect.Map && p3.Kind() != reflect.Slice {
+			panic("Third argument must be an *object, map, or slice")
+		}
+
+		fallthrough
+	case 2:
+		p1, p2 = typ.In(0), typ.In(1)
+		if "http.ResponseWriter" != p1.String() {
+			panic("First argument must be an http.ResponseWriter")
+		}
+
+		if "*http.Request" != p2.String() {
+			panic("Second argument must be a *http.Request")
+		}
+	default:
+		panic("Handler must have 2-3 arguments: ResponseWriter, Request, [Object]")
+	}
+
+	if typ.NumOut() != 2 {
+		panic("Handler must have two returns: *object or interface{}, and error")
+	}
+
+	o1, o2 := typ.Out(0), typ.Out(1)
+
+	if "interface {}" != o1.String() && o1.Kind() != reflect.Ptr && o1.Kind() != reflect.Slice && o1.Kind() != reflect.Map {
+		panic("First return must be an empty *object, map, slice or interface{}")
+	}
+
+	if "error" != o2.String() {
+		panic("Second return must be an error")
+	}
+
+	return &JSONHandler{fn: reflect.ValueOf(fn), in: p3}
+}
---
+++ json_test.go
@@ -0,0 +1,269 @@
+package jsonware
+
+import (
+	"bytes"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+type testType struct {
+	Name string `json:"name"`
+}
+
+type testController struct {
+	toReturn string
+}
+
+// param *
+func testHandler1(w http.ResponseWriter, r *http.Request, t *testType) (interface{}, error) {
+	return &testType{"hi"}, nil
+}
+
+// method instead of function
+func (s *testController) testHandler2(w http.ResponseWriter, r *http.Request) (interface{}, error) {
+	return &testType{s.toReturn}, nil
+}
+
+// return *
+func testHandler3(w http.ResponseWriter, r *http.Request, t *testType) (*testType, error) {
+	return &testType{"hi"}, nil
+}
+
+// param slice
+func testHandler4(w http.ResponseWriter, r *http.Request, t []*testType) (interface{}, error) {
+	return &testType{"hi"}, nil
+}
+
+// param map
+func testHandler5(w http.ResponseWriter, r *http.Request, t map[string]*testType) (interface{}, error) {
+	return &testType{"hi"}, nil
+}
+
+// return slice
+func testHandler6(w http.ResponseWriter, r *http.Request) ([]*testType, error) {
+	return []*testType{{"hi"}}, nil
+}
+
+// return map
+func testHandler7(w http.ResponseWriter, r *http.Request) (map[string]*testType, error) {
+	return map[string]*testType{"hi": &testType{"hi"}}, nil
+}
+
+// Params Arity
+func badHandler1() (interface{}, error) { return nil, nil }
+
+// Return arity
+func badHandler2(w http.ResponseWriter, r *http.Request) error { return nil }
+
+// 1st arg
+func badHandler3(w int, r *http.Request) (interface{}, error) { return nil, nil }
+
+// 2nd arg
+func badHandler4(w http.ResponseWriter, r int) (interface{}, error) { return nil, nil }
+
+// 3rd arg
+func badHandler5(w http.ResponseWriter, r *http.Request, t testType) (interface{}, error) {
+	return nil, nil
+}
+
+// 1st return
+func badHandler6(w http.ResponseWriter, r *http.Request) (testType, error) { return testType{}, nil }
+
+// 2nd return
+func badHandler7(w http.ResponseWriter, r *http.Request) (interface{}, int) { return nil, 5 }
+
+// 500 error
+func errHandler1(w http.ResponseWriter, r *http.Request) (interface{}, error) {
+	return nil, errors.New("error occurred")
+}
+
+// 200 JSONErr
+func errHandler2(w http.ResponseWriter, r *http.Request) (interface{}, error) {
+	return nil, JSONErr{error: errors.New("validation error")}
+}
+
+// handled json error
+func errHandler3(w http.ResponseWriter, r *http.Request) (interface{}, error) {
+	return nil, JSONErr{http.StatusBadRequest, errors.New("ugly request")}
+}
+
+func TestJSON_Serializing(t *testing.T) {
+	t.Parallel()
+
+	var tests = []struct {
+		handler interface{}
+		method  string
+		status  int
+		reqbody string
+		resbody string
+	}{
+		{testHandler1, "POST", 400, `{ "name"asonetd!!: "hi" }`, "could not deserialize"},
+		{testHandler1, "POST", 200, `{ "name": "hi" }`, "hi"},
+		{(&testController{"hello"}).testHandler2, "GET", 200, "", `{"name":"hello"}`},
+		{testHandler3, "POST", 200, `{ "name": "hi" }`, `hi`},
+		{testHandler4, "POST", 200, `[{ "name": "hi" }]`, `hi`},
+		{testHandler5, "POST", 200, `{ "friend": { "name": "hi" }}`, `hi`},
+		{testHandler6, "GET", 200, ``, `[{"name":"hi"}]`},
+		{testHandler7, "GET", 200, ``, `{"hi":{"name":"hi"}}`},
+	}
+
+	for i, test := range tests {
+		res := httptest.NewRecorder()
+		req, _ := http.NewRequest(test.method, "/", bytes.NewBufferString(test.reqbody))
+		req.Header = http.Header{"Accept": []string{"*/*"}}
+
+		j := JSON(test.handler)
+		j.ServeHTTP(res, req)
+
+		if res.Code != test.status {
+			t.Errorf("Test: %d", i)
+			t.Errorf("Expected status: %d, got: %d", test.status, res.Code)
+		}
+
+		if b := res.Body.String(); !strings.Contains(b, test.resbody) {
+			t.Errorf("Test: %d", i)
+			t.Errorf("Expected body: %s, got: %s", test.resbody, b)
+		}
+	}
+}
+
+func TestJSON_RequestFilter(t *testing.T) {
+	t.Parallel()
+
+	normHeader := http.Header{
+		"Accept": []string{"*/*"},
+	}
+	badAccept := http.Header{
+		"Accept": []string{"application/xml"},
+	}
+
+	var tests = []struct {
+		handler interface{}
+		method  string
+		status  int
+		headers http.Header
+		resbody string
+	}{
+		{testHandler1, "GET", 400, badAccept, "json-accepting"},
+		{testHandler1, "GET", 400, normHeader, "invalid http method"},
+		{testHandler1, "POST", 200, normHeader, "hi"},
+		{testHandler1, "PUT", 200, normHeader, "hi"},
+		{testHandler1, "PATCH", 200, normHeader, "hi"},
+		{(&testController{"hello"}).testHandler2, "GET", 200, normHeader, "hello"},
+		{(&testController{"hello"}).testHandler2, "POST", 400, normHeader, "invalid http method"},
+		{(&testController{"hello"}).testHandler2, "PUT", 400, normHeader, "invalid http method"},
+		{(&testController{"hello"}).testHandler2, "PATCH", 400, normHeader, "invalid http method"},
+	}
+
+	for i, test := range tests {
+		res := httptest.NewRecorder()
+		req, _ := http.NewRequest(test.method, "/", bytes.NewBufferString(`{ "name": "hi" }`))
+		req.Header = test.headers
+
+		j := JSON(test.handler)
+		j.ServeHTTP(res, req)
+
+		if res.Code != test.status {
+			t.Errorf("Test: %d", i)
+			t.Errorf("Expected status: %d, got: %d", test.status, res.Code)
+		}
+
+		if b := res.Body.String(); !strings.Contains(b, test.resbody) {
+			t.Errorf("Test: %d", i)
+			t.Errorf("Expected body: %s, got: %s", test.resbody, b)
+		}
+	}
+}
+
+func TestJSON_Errors(t *testing.T) {
+	t.Parallel()
+
+	var tests = []struct {
+		handler interface{}
+		status  int
+		resbody string
+		log     string
+	}{
+		{errHandler1, 500, "an internal server error", "internal error: error occurred"},
+		{errHandler2, 200, "validation error", ""},
+		{errHandler3, 400, "ugly request", ""},
+	}
+
+	log := &bytes.Buffer{}
+
+	for i, test := range tests {
+		res := httptest.NewRecorder()
+		req, _ := http.NewRequest("GET", "/", nil)
+		req.Header = http.Header{"Accept": []string{"*/*"}}
+
+		log.Reset()
+		j := JSON(test.handler).Log(log)
+		j.ServeHTTP(res, req)
+
+		if res.Code != test.status {
+			t.Errorf("Test: %d", i)
+			t.Errorf("Expected status: %d, got: %d", test.status, res.Code)
+		}
+
+		if b := res.Body.String(); !strings.Contains(b, test.resbody) {
+			t.Errorf("Test: %d", i)
+			t.Errorf("Expected body: %s, got: %s", test.resbody, b)
+		}
+
+		if l := log.String(); !strings.Contains(l, test.log) {
+			t.Errorf("Test: %d", i)
+			t.Errorf("Expected error: %s, got: %s", test.log, l)
+		}
+	}
+}
+
+func TestJSON_Panics(t *testing.T) {
+	t.Parallel()
+
+	var tests = []struct {
+		handler     interface{}
+		shouldPanic bool
+	}{
+		{testHandler1, false},
+		{(&testController{"hello"}).testHandler2, false},
+		{testHandler3, false},
+		{testHandler4, false},
+		{testHandler5, false},
+		{testHandler6, false},
+		{testHandler7, false},
+		{badHandler1, true},
+		{badHandler2, true},
+		{badHandler3, true},
+		{badHandler4, true},
+		{badHandler5, true},
+		{badHandler6, true},
+		{badHandler7, true},
+		{5, true},
+	}
+
+	for _, test := range tests {
+		if didPanic, msg := testPanic(test.handler); didPanic != test.shouldPanic {
+			t.Errorf("Test: %T", test.handler)
+			t.Errorf("Expected didPanic: %v, but got: %v", test.shouldPanic, didPanic)
+			if didPanic {
+				t.Errorf("Panic msg: %s", msg)
+			}
+		}
+	}
+}
+
+func testPanic(handler interface{}) (didPanic bool, msg string) {
+	defer func() {
+		err := recover()
+		didPanic = nil != err
+		if didPanic {
+			msg = err.(string)
+		}
+	}()
+
+	JSON(handler)
+	return
+}
